1f5495f8b4d2fb91231cb54be9c0134ef4e11f47
Grant Starnes <gstarnes@iu.edu>
2025-10-07 17:17:09 -0400

    On the right track (I think) to figuring out get_artist

--- apputil.py
+++ apputil.py
@@ -1,7 +1,7 @@
 # your code here ...
 from dotenv import load_dotenv
 import os
-
+import requests
 class Genius:
     def __init__(self, access_token=None):
         # Load environment variables
@@ -15,7 +15,23 @@ class Genius:
             raise ValueError("Genius access token must be provided or set in environment variables.")
 
         # Base URL and headers
-        self.base_url = "https://api.genius.com"
+        self.base_url = "http://api.genius.com"
         self.headers = {
             "Authorization": f"Bearer {self.access_token}"
         }
+
+    def get_artist(self, search_term):
+        search_url = f"{self.base_url}/search"
+        headers = self.headers
+        params = {'q': search_term}
+        response = requests.get(search_url, headers=headers, params=params)
+        json_data = response.json()
+    
+        api_path = json_data['response']['hits'][0]['result']['primary_artist']['api_path']
+        artist_id = api_path.split('/')[-1]
+    
+        artist_url = f"{self.base_url}{api_path}"
+        artist_response = requests.get(artist_url, headers=headers)
+        artist_data = artist_response.json()
+    
+        return artist_data
